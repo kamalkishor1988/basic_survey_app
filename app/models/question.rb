1e1f5bb3bf6c9403ee448d8c531a8d3fe86a0678
kamal.kishor <kamal.kishor@relyonsoft.com>
2019-09-23 17:24:14 +0530

    association modified

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :survey, optional: true
-  has_many :answers
+  has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers, allow_destroy: true
 end
